beeff0896b58e2bc8a35c514d41546df5bb5ac9c
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-08-25 09:32:38 +0200

    Add a new function for finding the nth term using an equation

--- assignment-3/q07.c
+++ assignment-3/q07.c
@@ -11,7 +11,8 @@
 
 #include <stdio.h>
 
-int findInSeries(int);
+int findInSeriesLoop(int);
+int findInSeriesEquation(int);
 
 int main(void){
     int number;
@@ -35,16 +36,33 @@ int main(void){
         printf("th");
         break;
     }
-    printf(" term of the series is %d", findInSeries(number));
-
+    printf(" term of the series is %d using loops method\n", findInSeriesLoop(number));
+    printf("& %d using equation method", findInSeriesLoop(number));
     return 0;
 }
 
-int findInSeries(int position){
+int findInSeriesLoop(int position){
     int counter = 1, ans = 1;
     while(counter != position){
         ans += 2;
         counter++;
     }
     return ans;
+}
+
+int findInSeriesEquation(int position){
+    /*
+    We know the Arithmetic Progression series is like = 2, 5, 8, 11, 14 …. … 
+    In this series 2 is the stating term of the series . 
+    Common difference = 5 – 2 = 3 (Difference common in the series). 
+    so we can write the series as :
+    t1 = a1 
+    t2 = a1 + (2-1) * d 
+    t3 = a1 + (3-1) * d 
+    . 
+    . 
+    . 
+    tN = a1 + (N-1) * d 
+    */
+    return 1 + (position - 1) * 2;
 }
